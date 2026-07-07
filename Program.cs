3ddafbd03f0b8dd1e2820c023d13f495d009c5bb
davieveraldo <0001236104@senaimgaluno.com.br>
2026-07-07 13:49:49 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[]vetor = new int[10];
+       int i=0;
+       int numPar = 0;
+
+        for(i=0; i < 10; i++)
+        {
+            Console.Write("Isira as posições do vetor: ");
+            vetor[i] = int.Parse(Console.ReadLine()); 
+
+            if(vetor[i] % 2 == 0)
+            {
+                numPar++;
+            }
+            
+        }
+        Console.WriteLine($"Números na posição {numPar} que são pares. ");
+
+        
+    }
+
+   
+}
